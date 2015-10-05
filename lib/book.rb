bf96d01093fd64cacdda69575098ac08f292cea2
mat-test111 <matbalez+test111@gmail.com>
2015-10-05 19:48:54 -0400

    Done.

---
+++ lib/book.rb
@@ -0,0 +1,21 @@
+class Book
+
+  GENRES = []
+
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
+
+  def initialize(title)
+    @title = title
+  end
+
+  def genre=(g)
+    @genre = g
+    GENRES << g
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+end

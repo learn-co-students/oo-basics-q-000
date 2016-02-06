aeff55b0cb17c67e1bf56282380c2594f733d8a9
alexgriff <alexalexgriffith@gmail.com>
2016-02-06 15:46:51 -0500

    Defined methods for Book class & Shoe class

---
+++ lib/book.rb
@@ -0,0 +1,21 @@
+
+class Book
+
+attr_accessor :title, :author, :page_count, :genre
+
+GENRES = []
+
+def initialize(title)
+  @title = title
+end
+
+def turn_page
+  puts "Flipping the page...wow, you read fast!"
+end
+
+def genre=(genre)
+  @genre = genre
+  GENRES << @genre
+end
+
+end
